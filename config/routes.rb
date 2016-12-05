6504cbcbf0fe0b5dab1e258313d3641914747503
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-05 12:20:39 +0000

    initial app setup with sign in/out and rspec setup

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    :registrations => "users/registrations"
+  }
+
+  devise_scope :user do
+  	root to: "devise/sessions#new"
+	end
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
