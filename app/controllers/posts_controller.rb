9753cdb7604342877ea9135537ae6c10e3c1ac1c
crispinandrews <crispin.andrews@gmail.com>
2016-12-06 10:21:06 +0000

    Added authenticate for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :require_permisson, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
