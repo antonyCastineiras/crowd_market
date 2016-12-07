bd95a49bdc2dfdb875e86e80c836c3e17fafe8ac
crispinandrews <crispin.andrews@gmail.com>
2016-12-07 11:47:38 +0000

    Able to upload images as a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  has_attached_file :image, styles: { large: '500x500>', medium: '300x300>', thumb: '100x100>' }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206163846) do
+ActiveRecord::Schema.define(version: 20161207110942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,13 @@ ActiveRecord::Schema.define(version: 20161206163846) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
