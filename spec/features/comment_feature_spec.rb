6135bb39f223c9ed8a813e359e5fe0cf349c0144
crispinandrews <crispin.andrews@gmail.com>
2016-12-07 12:04:25 +0000

    Change to comment feature test to comment via form. Removed public images folder for git commits

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -8,10 +8,8 @@ feature 'comments' do
 
   context 'creating comments' do
     it 'users can leave a comment' do
-      print page.body
-      click_link 'Comment'
       fill_in "comment_text", with: 'Testing'
-      click_button 'Leave Comment'
+      click_button 'Comment'
       expect(current_path).to eq '/posts'
       expect(page).to have_content('Testing')
     end
