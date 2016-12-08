974699f706b913d4c76846538e453dbf8cfb82ee
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-08 13:29:05 +0000

    set @user to current_user across all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_user
+
+  def set_user
+  	@user = current_user if current_user
+  end
 end
