09ee5f4c097661c7875fee52bb7ca3e790c6e49b
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-12 12:35:06 +0000

    can draw on posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209184131) do
+ActiveRecord::Schema.define(version: 20161211213219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adverts", force: :cascade do |t|
+    t.string   "width"
+    t.string   "height"
+    t.string   "left"
+    t.string   "top"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
