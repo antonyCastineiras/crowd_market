b7a69e803c41c82796a17a572d7b613f8aeb0ab7
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-12 20:30:35 +0000

    fixed bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209184131) do
+ActiveRecord::Schema.define(version: 20161212152710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adverts", force: :cascade do |t|
+    t.string   "width"
+    t.string   "height"
+    t.string   "left"
+    t.string   "top"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
@@ -42,6 +51,12 @@ ActiveRecord::Schema.define(version: 20161209184131) do
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "card_token"
   end
 
   create_table "posts", force: :cascade do |t|
