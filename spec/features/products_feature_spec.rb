dab9f98afd80d3e56d94b2972f85078648dc83d8
crispinandrews <crispin.andrews@gmail.com>
2016-12-13 16:34:39 +0000

    Images added to productts. Tests done for add and update product

--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -1,2 +1,26 @@
 require 'rails_helper'
 
+feature 'Products' do
+
+  context 'New products' do
+
+    before do
+      sign_up_seller('123@mail.com', 'abc123')
+    end
+
+    scenario 'Seller can add a product', js: true do
+      add_product
+      expect(page).to have_content('Product')
+      expect(page).to have_content('Product description')
+      expect(page).to have_content('Price: £500.0')
+    end
+
+    scenario 'user can edit post', js: true do
+      add_product
+      click_link 'Update this product'
+      fill_in 'product[product_name]', with: 'New product name'
+      click_button 'Update Product'
+      expect(page).to have_content('New product name')
+    end
+  end
+end
