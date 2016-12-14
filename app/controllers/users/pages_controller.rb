aaebe0daa276018e5660942acf60cf1a9adcb5d3
Michal Klesinski <michkles@Michals-MacBook-Pro.local>
2016-12-14 14:37:24 +0000

    friends posts are displayed by order(creation date or modification date)

--- app/controllers/users/pages_controller.rb
+++ app/controllers/users/pages_controller.rb
@@ -1,6 +1,10 @@
 class Users::PagesController < ApplicationController
 
   def show
+    @friends_posts = []
+    if current_user
+      get_posts_from_friends
+    end
     User.find(params[:id]) == current_user ? @user = current_user : @user = User.find(params[:id])
     @image = Image.new
     @friendship = Friendship.new
@@ -12,7 +16,19 @@ class Users::PagesController < ApplicationController
   	@seller = Seller.new
   end
 
+  private
+
   def page_params
   	params.require(:page).permit(:id)
   end
+
+  def get_posts_from_friends
+    current_user.friends.each do |friend|
+      friend.posts.each do |post|
+        @friends_posts.push(post)
+      end
+    end
+    @friends_posts = @friends_posts.sort_by { |post| [post.created_at, post.updated_at].max }.reverse!
+  end
+
 end
