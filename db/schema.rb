eeb575ad1095fd0feb62e95534722225f402c4c9
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-15 13:21:20 +0000

    fixed bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214180904) do
+ActiveRecord::Schema.define(version: 20161214210932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20161214180904) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "stripe_account_id"
     t.string   "shop_name"
     t.string   "first_name"
     t.string   "last_name"
