54c8fc520b7d229a64ea58896e29fa2d96a25c25
antonyCastineiras <antonycastineiras80@gmail.com>
2016-12-15 16:36:31 +0000

    displays whether a friend is online or not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214210932) do
+ActiveRecord::Schema.define(version: 20161215162029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20161214210932) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "stripe_account_id"
     t.string   "shop_name"
     t.string   "first_name"
     t.string   "last_name"
@@ -119,6 +118,7 @@ ActiveRecord::Schema.define(version: 20161214210932) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "last_seen"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
